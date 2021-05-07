0cad1bc64f8923c7687afa286e3b5369fbd96cfe
ZettaiVR <zettaivr@gmail.com>
2021-05-07 05:21:04 +0200

    First version

---
+++ Assets/Download.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections;
+using System.IO;
+using System.Text;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class Download : MonoBehaviour
+{
+    public IEnumerator DownloadBundle() 
+    {
+        if (Statics.GetFlag("-?"))  
+        {
+            Statics.PrintHelp();
+            yield break;
+        }
+        System.Diagnostics.Stopwatch stopwatch = new System.Diagnostics.Stopwatch();
+        stopwatch.Start();
+        string url = Statics.GetArg("-url");
+        string id = Statics.GetArg("-id");
+        string versionString = Statics.GetArg("-ver");
+        string path = Statics.GetArg("-path");
+        bool compressed = !Statics.GetFlag("-uncompressed");
+        uint version = 0;
+        if (!string.IsNullOrEmpty(versionString)) 
+        {
+            uint.TryParse(versionString, out version); 
+        }
+        if (string.IsNullOrEmpty(path))
+        {
+            path = Application.persistentDataPath + Path.DirectorySeparatorChar + "cache";
+        }
+        StringBuilder sb = new StringBuilder();
+        sb.Append("path: ");
+        sb.AppendLine(path);
+        sb.Append("id: ");
+        sb.AppendLine(id);
+        sb.Append("version: ");
+        sb.Append(version);
+        sb.AppendLine();
+        sb.Append("url: ");
+        sb.AppendLine(url);
+        sb.Append("compression: ");
+        sb.AppendLine(compressed ? "LZ4 compressed" : "uncompressed");
+        sb.AppendLine("Command line arguments:");
+        foreach (var item in Environment.GetCommandLineArgs())
+        {
+            sb.AppendLine(item);
+        }
+        Debug.Log(sb.ToString());
+        if (!string.IsNullOrEmpty(url) && !string.IsNullOrEmpty(id) && version > 0)
+        {
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+            Hash128 hash = new Hash128(0, 0, 0, version);
+            Cache newCache = Caching.AddCache(path);
+            if (newCache.valid)
+            {
+                Caching.compressionEnabled = compressed;
+                Caching.currentCacheForWriting = newCache;
+            }
+            string folder = Statics.GetHashFromString(id);
+            CachedAssetBundle cachedAssetBundle = new CachedAssetBundle(folder, hash);
+            UnityWebRequest request = UnityWebRequestAssetBundle.GetAssetBundle(url, cachedAssetBundle);
+            yield return request.SendWebRequest();
+            if (request.isNetworkError || request.isHttpError)
+            {
+                Debug.Log(request.error);
+                Console.WriteLine("Error! " + request.error);
+            }
+            stopwatch.Stop();
+            Debug.Log("Elapsed ticks: " + stopwatch.ElapsedTicks);
+            Application.Quit(1);
+        }
+        Application.Quit();
+    }
+    private void Start()
+    {
+        StartCoroutine(DownloadBundle());
+    }
+}
---
+++ Assets/Statics.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+using UnityEngine;
+
+public static class Statics
+{
+    public static void PrintHelp() 
+    {
+        Debug.Log($"{Application.productName} v{Application.version}. \r\nUsage:\r\n" +
+        " -url            URL to download\r\n" +
+        " -id             ID of the file\r\n" +
+        " -ver            file version\r\n" +
+        " -path           cache path\r\n" +
+        " -uncompressed   save bundles uncompressed");
+        Application.Quit();
+    }
+    public static string GetArg(string name)
+    {
+        var args = Environment.GetCommandLineArgs();
+        for (int i = 0; i < args.Length; i++)
+        {
+            if (args[i].Equals(name, StringComparison.OrdinalIgnoreCase) && args.Length > i + 1)
+            {
+                return args[i + 1];
+            }
+        }
+        return null;
+    }
+    public static bool GetFlag(string name)
+    {
+        var args = Environment.GetCommandLineArgs();
+        foreach (string arg in args)
+        {
+            if(arg.Equals(name, StringComparison.OrdinalIgnoreCase))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+    public static string GetHashFromString(string str)
+    {
+        return SHA256.Create().ComputeHash(Encoding.UTF8.GetBytes(str)).ToHex(true).Substring(0, 16);
+    }
+    public static string ToHex(this byte[] bytes, bool upperCase)
+    {
+        StringBuilder result = new StringBuilder(bytes.Length * 2);
+
+        for (int i = 0; i < bytes.Length; i++)
+            result.Append(bytes[i].ToString(upperCase ? "X2" : "x2"));
+
+        return result.ToString();
+    }
+}
